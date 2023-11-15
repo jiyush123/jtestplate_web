8a5664dc022db8fd3cec9cd310dd514c16c1ffb2
jianys <jianys@chinaunicom.cn>
2023-11-15 21:25:21 +0800

    封装axios并新增登录接口

---
+++ src/http/api.js
@@ -0,0 +1,171 @@
+import $http from './index.js'
+
+export const login = (data) => {
+    return $http({
+        method: "post",
+        url: "http://127.0.0.1:8000/login/",
+        params: {},
+        data: data,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
+// export const auth = (data) => {
+//     return $http({
+//         method: "post",
+//         url: "http://127.0.0.1:8000/auth/",
+//         params: {},
+//         data: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+export const logout = (data) => {
+    return $http({
+        method: "post",
+        url: "http://127.0.0.1:8000/logout/",
+        params: {},
+        data: data,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
+export const getUserList = (params) => {
+    return $http({
+        method: "get",
+        url: "http://127.0.0.1:8000/user/list/",
+        params: params,
+        data: {},
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
+// export const getUserInfo = (params) => {
+//     return $http({
+//         method: "get",
+//         url: "http://127.0.0.1:8000/user/detail/",
+//         params: params,
+//         data: {},
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+// export const addUser = (data) => {
+//     return $http({
+//         method: "post",
+//         url: "http://127.0.0.1:8000/user/add/",
+//         params: {},
+//         data: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+// export const delUser = (data) => {
+//     return $http({
+//         method: "post",
+//         url: "http://127.0.0.1:8000/user/del/",
+//         params: {},
+//         data: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+// export const updateUser = (data) => {
+//     return $http({
+//         method: "post",
+//         url: "http://127.0.0.1:8000/user/update/",
+//         params: {},
+//         data: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+
+// // API
+// export const getAPIList = (params) => {
+//     return $http({
+//         method: "get",
+//         url: "http://127.0.0.1:8000/api/list/",
+//         params: params,
+//         data: {},
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+// export const getAPIInfo = (params) => {
+//     return $http({
+//         method: "get",
+//         url: "http://127.0.0.1:8000/api/detail/",
+//         params: params,
+//         data: {},
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+// export const addAPI = (data) => {
+//     return $http({
+//         method: "post",
+//         url: "http://127.0.0.1:8000/api/addsave/",
+//         params: {},
+//         data: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+// export const editAPI = (data) => {
+//     return $http({
+//         method: "post",
+//         url: "http://127.0.0.1:8000/api/update/",
+//         params: {},
+//         data: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+// export const delAPI = (data) => {
+//     return $http({
+//         method: "post",
+//         url: "http://127.0.0.1:8000/api/del/",
+//         params: {},
+//         data: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
+
+// export const debugAPI = (data) => {
+//     return $http({
+//         method: "post",
+//         url: "http://127.0.0.1:8000/api/test/",
+//         params: {},
+//         data: data,
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     });
+// }
---
+++ src/http/index.js
@@ -0,0 +1,53 @@
+import router from '@/router';
+import axios from 'axios';
+import { ElMessage } from 'element-plus'
+
+
+// 创建了一个axios实例,给这个实例编写配置，后续所有通过实例发送的请求，都受当前配置约束
+const $http = axios.create({
+    baseURL: "http://127.0.0.1:8000",
+    timeout: 1000,
+    headers:{'Content-Type':'application/json'}
+});
+// 请求拦截器
+$http.interceptors.request.use(function(config){
+    // 在发请求前做什么
+    console.log(config.url);
+    // 如果是登录退出，不需要添加token
+    if (config.url.includes('login') || config.url.includes('logout')){
+        return config
+    }
+    else{
+        // 其它页面添加请求头访问
+        console.log('添加请求头token');
+        config.headers['Authorization']=localStorage.getItem('token');
+        return config;
+    }
+},function(error){
+    // 对请求错误做什么
+    return Promise.reject(error);
+});
+
+// 添加响应拦截器
+$http.interceptors.response.use(function(response){
+    // 对响应数据做什么
+    let data = response.data;
+
+    // 如果请求返回401，则未登录
+    if(data.code == 401){
+        ElMessage({
+            showClose: true,
+            center: true,
+            message: data.msg,
+            type: 'error',
+        })
+        router.push('/login');
+        
+    }
+    return data;
+},function(error){
+    // 对响应报错做什么
+    return Promise.reject(error);
+});
+
+export default $http
