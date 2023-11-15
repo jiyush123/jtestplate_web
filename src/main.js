1093b7d11c373c00134cef555ba77dd9a386388b
jianys <jianys@chinaunicom.cn>
2023-11-15 21:24:43 +0800

    修改主文件信息

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App);
+app.use(store).use(router).use(ElementPlus,{locale: zhCn,}).mount('#app')
