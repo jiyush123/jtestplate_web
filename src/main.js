63a7a64980fb48000080e1c78761e15ff1fcea56
jianys <jianys@chinaunicom.cn>
2023-11-27 23:30:09 +0800

    没变

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// import { VueDraggableNext as Draggable } from 'vue-draggable-next'
+// import {draggable} from 'vue-draggable';
 
 
 const app = createApp(App);
