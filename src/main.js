984736e6c7e377786de36345d277eafd92a6f31f
jianys <jianys@chinaunicom.cn>
2024-01-30 11:02:57 +0800

    增加图标注册

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 app.use(store).use(router).use(ElementPlus,{locale: zhCn,}).mount('#app')
