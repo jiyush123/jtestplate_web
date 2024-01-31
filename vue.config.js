416d64fa1e422e086104408939ebaf27d2d609a5
jianys <jianys@chinaunicom.cn>
2024-01-31 16:48:00 +0800

    增加monaco编辑器配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ['python','javascript'], // 添加支持的语言列表
+      }),
+    ],
+  },
+});
