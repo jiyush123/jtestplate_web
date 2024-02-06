9b01e657b9ede40e8e194464c868a111a80801a3
jianys <jianys@chinaunicom.cn>
2024-02-06 17:17:20 +0800

    完成编辑器数据双向绑定

--- src/views/MonacoEdit.vue
+++ src/views/MonacoEdit.vue
@@ -1,55 +1,52 @@
 <template>
-    <div ref="editorContainer" style="height: 300px; width: 80%;margin:auto;border: 1px solid #a8abb2;" ></div>
-    <el-button @click="clickbtn">点击</el-button>
-    <el-button @click="showdata">回显json</el-button>
+  <div ref="editorContainer" style="height: 300px; width: 90%;margin:auto;border: 1px solid #a8abb2;"></div>
 </template>
   
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, defineProps, watch,defineEmits } from 'vue';
 import * as monaco from 'monaco-editor';
 
 const editorContainer = ref(null);
 
-// 假设你有一个JSON对象或字符串
-const jsonData = {
-  "name": "John Doe",
-  "age": 30,
-  "city": "New York"
-};
+const props = defineProps({
+  code: String,
+})
 
-// 将JSON对象转换为JSON字符串
-const jsonStr = JSON.stringify(jsonData, null, 2); // 第二个和第三个参数用于美化输出，添加缩进和换行
-
-// 回显数据到编辑器的方法
-const showdata=()=>{
-    editorInstance.setValue(jsonStr);
-    // 设置只读方法
-    editorInstance.updateOptions({ readOnly: true });
-}
 
 let editorInstance;
+const emits = defineEmits([
+  'update:code'
+])
 
-const clickbtn=()=> {
-  if (editorInstance) {
-    // 获取编辑器内容的方法
-    let currentValue = editorInstance.getValue();
-    console.log(currentValue);
-    // 或者可以将这个值用于其他用途
-  } else {
-    console.error('Monaco Editor instance not found');
-  }
-}
+
+// defineExpose({
+//   getData: () => editorInstance.getValue(),
+//   setData: (newValue) => editorInstance.setValue(newValue)
+// })
+
 
 onMounted(() => {
+  if (editorContainer.value) {
+    editorInstance = monaco.editor.create(editorContainer.value, {
+      value: props.code,
+      language: 'python',
+      // theme: 'vs-dark', // 可选，设置主题，黑色
+      theme: 'vs',
+      automaticLayout: true, // 可选，自动调整编辑器大小适应容器
+    });
+  }
+  // 监听编辑器内容变化，触发事件，使用编辑器内容更新到code，既父组件传过来的表单数据
+  editorInstance.onDidChangeModelContent(() => {
+    emits('update:code', editorInstance.getValue())
+  });
 
-    if (editorContainer.value) {
-        editorInstance = monaco.editor.create(editorContainer.value, {
-            value: `# Write your Python code here\nprint("Hello, World!")`,
-            language: 'python',
-            // theme: 'vs-dark', // 可选，设置主题，黑色
-            theme:'vs',
-            automaticLayout: true, // 可选，自动调整编辑器大小适应容器
-        });
-    }
 });
+// 监听父组件的code有没有变化，如果发生了改变并且内容与当前内容不一致，更新到编辑器内容里面
+watch(() => props.code , (newCode) => {
+  if(!newCode || !editorInstance) return
+  if(newCode === editorInstance.getValue() ) return
+  editorInstance.setValue(newCode)
+})
+
+
 </script>
