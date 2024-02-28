b1d56c49c455307adae8001db18b68c15f2020df
jianys <jianys@chinaunicom.cn>
2024-02-28 17:31:34 +0800

    新增断言子组件

---
+++ src/views/RequestAssert.vue
@@ -0,0 +1,159 @@
+<template>
+    <el-table :data="assertData" border style="width: 100%">
+        <el-table-column prop="assertkey" label="响应值(JsonPath)" width='250'>
+            <template #default="scope">
+                <el-input v-model="assertData[scope.$index].assertkey" @input="emit('update:assert', changeAssert())" />
+            </template>
+        </el-table-column>
+        <el-table-column prop="assertvalue" label="期望值">
+            <template #default="scope">
+                <el-input v-model="assertData[scope.$index].assertvalue" class="input-with-select"
+                    @input="emit('update:assert', changeAssert())">
+                    <template #prepend>
+                        <el-select v-model="assertData[scope.$index].assertDataType">
+                            <el-option label="string" value="string" @input="emit('update:assert', changeAssert())" />
+                            <el-option label="int" value="int" @input="emit('update:assert', changeAssert())" />
+                            <el-option label="bool" value="bool" @input="emit('update:assert', changeAssert())" />
+                        </el-select>
+                    </template>
+                </el-input>
+            </template>
+        </el-table-column>
+        <el-table-column prop="assertdecription" label="描述">
+            <template #default="scope">
+                <el-input v-model="assertData[scope.$index].assertdecription"
+                    @input="emit('update:assert', changeAssert())" />
+            </template>
+        </el-table-column>
+        <el-table-column prop="assertresult" label="结果" width="100">
+            <template #default="scope">
+                <el-input v-model="assertData[scope.$index].assertresult" disabled />
+            </template>
+        </el-table-column>
+        <el-table-column width='100'>
+            <template #default="scope">
+                <el-button type="danger" @click="delAssert(scope.$index)">删除</el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+    <el-button type="primary" @click="addAssert" style="margin-top: 10px;">新增assert参数</el-button>
+</template>
+<style>
+.input-with-select .el-input-group__prepend {
+    background-color: var(--el-fill-color-blank);
+}
+
+.input-with-select .el-select {
+    width: 85px;
+}
+</style>
+<script setup>
+import { reactive, defineProps, defineEmits, defineExpose, onMounted } from 'vue';
+// 接收父组件传过来的数据结构addForm.steps[i].assert  {key:{value:xxx,des:xxx},key:{value:xxx,des:xxx}}
+
+const props = defineProps({
+    assert: Object,
+})
+const emit = defineEmits(['update:assert'])
+
+const assertData = reactive(
+    []
+);
+
+// 创建一个数据类型作映射
+let data_type = new Map();
+
+// 添加键值对，让获取的数据类型匹配列表选项
+data_type.set('number', 'int');
+data_type.set('string', 'string');
+data_type.set('boolean', 'bool');
+
+
+const addAssert = () => {
+    assertData.push({
+        assertDataType: 'string'
+    })
+}
+
+const delAssert = (delindex) => {
+    assertData.splice(delindex, 1);
+    emit('update:assert', changeAssert())
+}
+
+const getAssert = () => {
+    assertData.length = 0;
+    for (let key in props.assert) {
+        let value = props.assert[key];
+        assertData.push({
+            assertkey: key,
+            assertDataType: data_type.get(typeof (value.value)),
+            assertvalue: value.value,
+            assertdecription: value.decription,
+        });
+    }
+}
+
+const assertResult = () => {
+    for (let key in props.assert) {
+        for (let i = 0; i < assertData.length; i++) {
+            if (assertData[i].assertkey === key) {
+                assertData[i].assertresult = props.assert[key].result;
+                break; // 找到一项匹配就退出循环
+            }
+        }
+    }
+}
+
+const changeAssert = () => {
+    let assert = {};
+
+    if (assertData.length === 0) {
+        assert = null
+    }
+    // 如果需要一个{}，则添加一个不输入key的参数
+    else if (assertData.length === 1 && assertData[0].assertkey === undefined) {
+        assert = {}
+    }
+    else {
+        for (let i = 0; i < assertData.length; i++) {
+            assert[assertData[i].assertkey] = { "datatype": assertData[i].assertDataType, "value": assertData[i].assertvalue, "decription": assertData[i].assertdecription };
+        }
+    }
+    return assert
+}
+
+const formatAssert = () => {
+    let assert = {};
+    if (assertData.length === 0) {
+        assert = null
+    }
+    else {
+        assert = props.assert;
+        if (Object.keys(assert).length > 0) {
+            for (let key in assert) {
+                if (assert[key].datatype === 'int') {
+                    assert[key].value = Number(assert[key].value);
+                }
+                else if (assert[key].datatype === 'bool') {
+                    if (assert[key].value === 'false') {
+                        assert[key].value = false;
+                    }
+                    else {
+                        assert[key].value = true;
+                    }
+                }
+                assert[key] = { "value": assert[key].value, "decription": assert[key].decription };
+            }
+        }
+    }
+    return assert
+}
+
+defineExpose({ getAssert, formatAssert, assertResult })
+
+onMounted(() => {
+    getAssert();
+    setTimeout(() => {
+    }, 1000)
+})
+</script>
