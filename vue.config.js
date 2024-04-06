e221d439bcfc19ba6a9d6a036fd01fc91fb4acf2
jianys <jianys@chinaunicom.cn>
2024-04-06 17:35:22 +0800

    禁用webpack内置的错误覆盖层

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
 
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
+    devServer: {
+      client: {
+        overlay: false, // 禁用内置错误覆盖层
+      },
+    },
     plugins: [
       new MonacoWebpackPlugin({
         languages: ['python','javascript'], // 添加支持的语言列表
