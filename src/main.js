240ce88178a5491ef4a18bd9e07600403a787f73
jianys <jianys@chinaunicom.cn>
2024-04-06 17:34:49 +0800

    增加全局错误提示处理

--- src/main.js
+++ src/main.js
@@ -6,10 +6,23 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { ElMessage } from 'element-plus'; // 引入 Element Plus 的消息提示组件
 
 
 const app = createApp(App);
+
+app.config.errorHandler = (err, instance, info) => {
+  // 向追踪服务报告错误，全局错误处理
+  console.log(err, instance, info);
+  ElMessage({
+    showClose: true,
+    center: true,
+    message: err,
+    type: 'error',
+  })
+}
+
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-  }
-app.use(store).use(router).use(ElementPlus,{locale: zhCn,}).mount('#app')
+  app.component(key, component)
+}
+app.use(store).use(router).use(ElementPlus, { locale: zhCn, }).mount('#app')
